884a3e703d7c12c27c6565778c309f91d76666fd
fwdew <38342596+fwdew@users.noreply.github.com>
2018-04-14 00:59:48 +0300

    Recreate repository

---
+++ CleanSolution/CleanForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CleanSolution
+{
+   partial class CleanForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null)) {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.buttonClean = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // buttonClean
+         // 
+         this.buttonClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.buttonClean.Location = new System.Drawing.Point(31, 12);
+         this.buttonClean.Name = "buttonClean";
+         this.buttonClean.Size = new System.Drawing.Size(210, 61);
+         this.buttonClean.TabIndex = 0;
+         this.buttonClean.Text = "Clean";
+         this.buttonClean.UseVisualStyleBackColor = true;
+         this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+         // 
+         // CleanForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(271, 87);
+         this.Controls.Add(this.buttonClean);
+         this.Name = "CleanForm";
+         this.Text = "Clean Solution";
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+      private System.Windows.Forms.Button buttonClean;
+   }
+}
+
---
+++ CleanSolution/CleanForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CleanSolution
+{
+   public partial class CleanForm : Form
+   {
+      private readonly string[] FOLDERS_TO_DELETE = {
+         ".vs",
+         "bin",
+         "obj",
+         "debug",
+         "release"
+      };
+
+      public CleanForm()
+      {
+         InitializeComponent();
+      }
+
+      private string GetFolderPathToClean()
+      {
+         using (var fbd = new FolderBrowserDialog()) {
+            var result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath)) {
+               return fbd.SelectedPath;
+            }
+         }
+
+         return string.Empty;
+      }
+
+      private void CleanFolderRecursively(string folderName)
+      {
+         var subFolders = Directory.GetDirectories(folderName);
+         for (var i = 0; i < subFolders.Length; i++) {
+            var subFolder = subFolders[i];
+            var subFolderName = Path.GetFileName(subFolder).ToLower();
+
+            if (FOLDERS_TO_DELETE.Contains(subFolderName)) {
+               Directory.Delete(subFolder, true);
+            } else {
+               CleanFolderRecursively(subFolder);
+            }
+         }
+      }
+
+      private void buttonClean_Click(object sender, EventArgs e)
+      {
+         var mainFolder = GetFolderPathToClean().Trim();
+
+         if (!string.IsNullOrEmpty(mainFolder)
+            && Directory.Exists(mainFolder)) {
+            CleanFolderRecursively(mainFolder);
+            Close();
+         }
+      }
+   }
+}
